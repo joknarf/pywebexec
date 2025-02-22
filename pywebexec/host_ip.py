a0e66694c7db3b5ef2e4742016ae4e59efcb1d8c
joknarf <joknarf@free.fr>
2025-02-22 12:32:36 +0100

    use scapy for host ip

--- pywebexec/host_ip.py
+++ pywebexec/host_ip.py
@@ -1,7 +1,7 @@
 import subprocess
 import ipaddress
 import platform
-import psutil
+from scapy.all import *
 from socket import gethostname, gethostbyname_ex, gethostbyaddr, inet_aton, inet_ntoa, AF_INET, SOCK_STREAM
 
 def resolve_hostname(host):
@@ -39,72 +39,24 @@ def resolve(host_or_ip):
     return resolve_hostname(host_or_ip)
 
 
-def get_default_gateway():
-    system = platform.system()
-    try:
-        if system == "Linux":
-            result = subprocess.run(['ip', 'route', 'show', 'default'], capture_output=True, text=True)
-            lines = result.stdout.splitlines()
-            for line in lines:
-                parts = line.split()
-                if 'via' in parts:
-                    return parts[2]
-        elif system == "Darwin":
-            result = subprocess.run(['netstat', '-nr', '-f', 'inet'], capture_output=True, text=True)
-            lines = result.stdout.splitlines()
-            for line in lines:
-                if 'default' in line and 'UTS' not in line:
-                    parts = line.split()
-                    if is_ip(parts[1]):
-                        return parts[1]
-    except Exception as e:
-        pass
-    return None
+def get_host_ip_scapy():
+    ip = scapy.all.get_if_addr(scapy.all.conf.iface)
+    if ip == '0.0.0.0':
+        nics = scapy.all.conf.ifaces
+        for nic in nics:
+            ip = scapy.all.get_if_addr(nic)
+            if ip not in ['127.0.0.1', '0.0.0.0']:
+                break
+    if ip == '0.0.0.0':
+        ip = '127.0.0.1'
+    return ip
 
-def netmask_to_cidr(netmask):
-    # Octet mask to CIDR
-    octets = netmask.split('.')
-    binary_str = ''.join(f'{int(octet):08b}' for octet in octets)
-    cidr = binary_str.count('1')
-    return cidr
-
-def _netmask_to_cidr(netmask):
-    # hex mask to CIDR
-    return sum([bin(int(h, 16)).count('1') for h in netmask.split('.')])
-
-
-def get_local_networks():
-    networks = []
-    neti_dict = psutil.net_if_addrs()
-    for snic_list in neti_dict.values():
-        for snic in snic_list:
-            if snic.family.name == 'AF_INET':
-                prefixlen = netmask_to_cidr(snic.netmask)
-                network = ipaddress.IPv4Network(f"{snic.address}/{prefixlen}", strict=False)
-                networks.append((snic.address, network))
-    return networks
-
-
-def is_gateway_in_network(gateway_ip, network):
-    return ipaddress.IPv4Address(gateway_ip) in network
-
-def get_host_ip_from_gateway():
-    gateway = get_default_gateway()
-    if gateway:
-        local_networks = get_local_networks()
-        for ip, network in local_networks:
-            if is_gateway_in_network(gateway, network):
-                return ip
-        else:
-            return '127.0.0.1'
-    else:
-        return '127.0.0.1'
 
 def get_host_ip(host_or_ip='0.0.0.0'):
     if host_or_ip == '0.0.0.0':
         (hostname, ip) = resolve(gethostname())
         if ip == '127.0.0.1':
-            (hostname, ip) = resolve(get_host_ip_from_gateway())
+            (hostname, ip) = resolve(get_host_ip_scapy())
     else:
         (hostname, ip) = resolve(host_or_ip)
     return (hostname, ip)
